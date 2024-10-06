8a1b8e9ed43978e8e8ea95225ccdbefbf9dba1f2
kennerm1 <123021312+kennerm1@users.noreply.github.com>
2024-10-06 15:53:23 -0700

    Raccoons

---
+++ RacoonStorm/Assets/Scripts/GameController.cs
@@ -0,0 +1,253 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+//using System;
+
+namespace SpatialPartitionPattern
+{
+    public class GameController : MonoBehaviour
+    {
+        public GameObject friendlyObj;
+        public GameObject enemyObj;
+
+        //Change materials to detect which enemy is the closest
+        public Material enemyMaterial;
+        public Material closestEnemyMaterial;
+
+        //To get a cleaner workspace, parent all soldiers to these empty gameobjects
+        public Transform enemyParent;
+        public Transform friendlyParent;
+
+        //Store all soldiers in these lists
+        List<Soldier> enemySoldiers = new List<Soldier>();
+        List<Soldier> friendlySoldiers = new List<Soldier>();
+
+        //Save the closest enemies to easier change back its material
+        List<Soldier> closestEnemies = new List<Soldier>();
+
+        //Grid data
+        float mapWidth = 50f;
+        int cellSize = 10;
+
+        //Number of soldiers on each team
+        int numberOfSoldiers = 100;
+
+        //The Spatial Partition grid
+        Grid grid;
+
+        //GUI
+        public bool enableSP = false;
+        public TMP_Text timeText;
+        public GameObject spEnabled;
+        public GameObject spDisabled;
+
+        public AudioSource yippee;
+        public AudioSource yippee2;
+        public AudioSource yippee3;
+        public AudioSource yippee4;
+        public AudioSource music;
+
+
+        void Start()
+        {
+            yippee.Play();
+            StartCoroutine(PlaySoundAfterDelay(yippee2, 500.0f));
+            music.Play();
+
+            //Create a new grid
+            grid = new Grid((int)mapWidth, cellSize);
+
+            //Add random enemies and friendly and store them in a list
+            for (int i = 0; i < numberOfSoldiers; i++)
+            {
+                //Give the enemy a random position
+                Vector3 randomPos = new Vector3(Random.Range(0f, mapWidth), 0.5f, Random.Range(0f, mapWidth));
+
+                //Create a new enemy
+                GameObject newEnemy = Instantiate(enemyObj, randomPos, Quaternion.identity) as GameObject;
+
+                //Add the enemy to a list
+                enemySoldiers.Add(new Enemy(newEnemy, mapWidth, grid));
+
+                //Parent it
+                newEnemy.transform.parent = enemyParent;
+
+
+                //Give the friendly a random position
+                randomPos = new Vector3(Random.Range(0f, mapWidth), 0.5f, Random.Range(0f, mapWidth));
+
+                //Create a new friendly
+                GameObject newFriendly = Instantiate(friendlyObj, randomPos, Quaternion.identity) as GameObject;
+
+                //Add the friendly to a list
+                friendlySoldiers.Add(new Friendly(newFriendly, mapWidth));
+
+                //Parent it 
+                newFriendly.transform.parent = friendlyParent;
+            }
+        }
+
+
+        void Update()
+        {
+            float startTime = Time.realtimeSinceStartup;
+            //Move the enemies
+            for (int i = 0; i < enemySoldiers.Count; i++)
+            {
+                enemySoldiers[i].Move();
+            }
+
+            //Reset material of the closest enemies
+            for (int i = 0; i < closestEnemies.Count; i++)
+            {
+                closestEnemies[i].soldierMeshRenderer.material = enemyMaterial;
+            }
+
+            //Reset the list with closest enemies
+            closestEnemies.Clear();
+
+            //For each friendly, find the closest enemy and change its color and chase it
+            for (int i = 0; i < friendlySoldiers.Count; i++)
+            {
+                //Soldier closestEnemy = FindClosestEnemySlow(friendlySoldiers[i]);
+
+                //The fast version with spatial partition
+                Soldier closestEnemy;
+
+                if (enableSP)
+                {
+                    closestEnemy = grid.FindClosestEnemy(friendlySoldiers[i]);
+                    spEnabled.SetActive(true);
+                    spDisabled.SetActive(false);
+                }
+                else
+                {
+                    closestEnemy = FindClosestEnemySlow(friendlySoldiers[i]);
+                    spEnabled.SetActive(false);
+                    spDisabled.SetActive(true);
+                }
+
+
+                //If we found an enemy
+                if (closestEnemy != null)
+                {
+                    //Change material
+                    closestEnemy.soldierMeshRenderer.material = closestEnemyMaterial;
+
+                    closestEnemies.Add(closestEnemy);
+
+                    //Move the friendly in the direction of the enemy
+                    friendlySoldiers[i].Move(closestEnemy);
+                }
+                float totalTime = Time.realtimeSinceStartup - startTime;
+                timeText.text = totalTime.ToString();
+            }
+        }
+
+
+        //Find the closest enemy - slow version
+        Soldier FindClosestEnemySlow(Soldier soldier)
+        {
+            Soldier closestEnemy = null;
+
+            float bestDistSqr = Mathf.Infinity;
+
+            //Loop thorugh all enemies
+            for (int i = 0; i < enemySoldiers.Count; i++)
+            {
+                //The distance sqr between the soldier and this enemy
+                float distSqr = (soldier.soldierTrans.position - enemySoldiers[i].soldierTrans.position).sqrMagnitude;
+
+                //If this distance is better than the previous best distance, then we have found an enemy that's closer
+                if (distSqr < bestDistSqr)
+                {
+                    bestDistSqr = distSqr;
+
+                    closestEnemy = enemySoldiers[i];
+                }
+            }
+
+            return closestEnemy;
+        }
+
+        public void toggleSP()
+        {
+            if (enableSP == false)
+            {
+                enableSP = true;
+            }
+            else if (enableSP == true)
+            {
+                enableSP = false;
+            }
+        }
+
+        public void addOneHundred()
+        {
+            numberOfSoldiers += numberOfSoldiers + 100;
+            for (int i = 0; i < numberOfSoldiers; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(0f, mapWidth), Random.Range(0.5f, mapWidth), Random.Range(0f, mapWidth));
+                GameObject newEnemy = Instantiate(enemyObj, randomPos, Quaternion.identity) as GameObject;
+                enemySoldiers.Add(new Enemy(newEnemy, mapWidth, grid));
+                newEnemy.transform.parent = enemyParent;
+
+                randomPos = new Vector3(Random.Range(0f, mapWidth), Random.Range(0.5f, mapWidth), Random.Range(0f, mapWidth));
+                GameObject newFriendly = Instantiate(friendlyObj, randomPos, Quaternion.identity) as GameObject;
+                friendlySoldiers.Add(new Friendly(newFriendly, mapWidth));
+                newFriendly.transform.parent = friendlyParent;
+            }
+        }
+
+        public void addOneThousand()
+        {
+            numberOfSoldiers += numberOfSoldiers + 1000;
+            for (int i = 0; i < numberOfSoldiers; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(0f, mapWidth), Random.Range(0.5f, mapWidth), Random.Range(0f, mapWidth));
+                GameObject newEnemy = Instantiate(enemyObj, randomPos, Quaternion.identity) as GameObject;
+                enemySoldiers.Add(new Enemy(newEnemy, mapWidth, grid));
+                newEnemy.transform.parent = enemyParent;
+
+                randomPos = new Vector3(Random.Range(0f, mapWidth), Random.Range(0.5f, mapWidth), Random.Range(0f, mapWidth));
+                GameObject newFriendly = Instantiate(friendlyObj, randomPos, Quaternion.identity) as GameObject;
+                friendlySoldiers.Add(new Friendly(newFriendly, mapWidth));
+                newFriendly.transform.parent = friendlyParent;
+            }
+        }
+
+        public void addOne()
+        {
+            numberOfSoldiers += numberOfSoldiers + 1;
+            for (int i = 0; i < numberOfSoldiers; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(0f, mapWidth), Random.Range(0.5f, mapWidth), Random.Range(0f, mapWidth));
+                GameObject newEnemy = Instantiate(enemyObj, randomPos, Quaternion.identity) as GameObject;
+                enemySoldiers.Add(new Enemy(newEnemy, mapWidth, grid));
+                newEnemy.transform.parent = enemyParent;
+
+                randomPos = new Vector3(Random.Range(0f, mapWidth), Random.Range(0.5f, mapWidth), Random.Range(0f, mapWidth));
+                GameObject newFriendly = Instantiate(friendlyObj, randomPos, Quaternion.identity) as GameObject;
+                friendlySoldiers.Add(new Friendly(newFriendly, mapWidth));
+                newFriendly.transform.parent = friendlyParent;
+            }
+        }
+
+        IEnumerator PlaySoundAfterDelay(AudioSource audioSource, float delay)
+        {
+            if (audioSource == null)
+            {
+                Debug.Log("No audio found");
+                yield break;
+            }
+
+            else
+            {
+                yield return new WaitForSeconds(delay);
+                audioSource.Play();
+            }
+        }
+
+    }
+}
